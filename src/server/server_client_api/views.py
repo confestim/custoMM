e652c25709a96bed121faa67240519dcafe0c7e7
confestim <confestim@wind.ows>
2023-05-09 04:42:52 +0200

    testing

--- src/server/server_client_api/views.py
+++ src/server/server_client_api/views.py
@@ -38,7 +38,7 @@ class PlayerViewSet(viewsets.ModelViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST', "PUT", "DELETE"])
-def current(request, creator):
+def current(request, creator=None):
     """
     Creates/edits/deletes the current game that's being played and orchestrates
     the matchmaking process.
