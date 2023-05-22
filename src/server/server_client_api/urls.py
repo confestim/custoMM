bcd6e8be45e20b09ecbeda5ede24e514908d4f2e
Boyan <boyan@bobokara.com>
2023-05-22 03:39:10 +0300

    Server updating

--- src/server/server_client_api/urls.py
+++ src/server/server_client_api/urls.py
@@ -5,10 +5,10 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'players', views.PlayerViewSet)
 router.register(r'current', views.CurrentViewSet)
+router.register(r'games', views.games)
+router.register(r'game', views.game)
 
 urlpatterns = [
-    path('game/', views.game),
-    path('games/', views.games),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
