359dfcbf98c840c673fcb3b4186ad7e2d9afef83
Boyan <boyan@bobokara.com>
2023-05-22 03:41:24 +0300

    Yeet

--- src/server/server_client_api/urls.py
+++ src/server/server_client_api/urls.py
@@ -5,10 +5,10 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'players', views.PlayerViewSet)
 router.register(r'current', views.CurrentViewSet)
-router.register(r'games', views.games)
-router.register(r'game', views.game)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/games/', views.games),
+    path('api/game/', views.game),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
