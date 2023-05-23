66504294e643508d555f31ec7b5b0256a95a5cc2
boyan <36108495+confestim@users.noreply.github.com>
2023-05-24 02:02:51 +0300

    DEBUG

--- src/discord/classes/Target.py
+++ src/discord/classes/Target.py
@@ -51,8 +51,8 @@ class Target:
         await self.ctx.send(self.bot)
         # Declaring channels
         team_1 = self.bot.get_channel(self.team_1)
-    
         team_2 = self.bot.get_channel(self.team_2)
+        print(team_1, team_2)
         # Embedding
         one_em = discord.Embed(title=f"Team 1", colour=discord.Colour(0x8c0303))
         two_em = discord.Embed(title=f"Team 2", colour=discord.Colour(0x0B5394))
